36575b40b90e9b6e016fd0b3c035c2646c031a9c
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-07-18 21:10:42 +1200

    Make use of EventExtractor in NotiEventExtractorService.kt

--- app/src/main/kotlin/com/boswelja/autoevent/notificationeventextractor/NotiEventExtractorService.kt
+++ app/src/main/kotlin/com/boswelja/autoevent/notificationeventextractor/NotiEventExtractorService.kt
@@ -13,55 +13,28 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.content.getSystemService
 import com.boswelja.autoevent.R
-import com.google.mlkit.common.model.DownloadConditions
-import com.google.mlkit.nl.entityextraction.DateTimeEntity
-import com.google.mlkit.nl.entityextraction.Entity
-import com.google.mlkit.nl.entityextraction.EntityAnnotation
-import com.google.mlkit.nl.entityextraction.EntityExtraction
-import com.google.mlkit.nl.entityextraction.EntityExtractionParams
-import com.google.mlkit.nl.entityextraction.EntityExtractor
-import com.google.mlkit.nl.entityextraction.EntityExtractorOptions
+import com.boswelja.autoevent.eventextractor.EventDetails
+import com.boswelja.autoevent.eventextractor.EventExtractor
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import java.text.DateFormat
-import java.util.Date
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 
 class NotiEventExtractorService : NotificationListenerService() {
 
     private val idCounter = AtomicInteger()
     private val coroutineScope = CoroutineScope(Dispatchers.Default)
-    private val modelDownloaded = MutableStateFlow(false)
 
+    private lateinit var eventExtractor: EventExtractor
     private lateinit var notificationManager: NotificationManager
-    private lateinit var eventExtractor: EntityExtractor
 
     override fun onListenerConnected() {
-        Log.i("NotiListenerService", "Listener connected")
+        Log.i("NotiEventExtractorService", "Listener connected")
+        eventExtractor = EventExtractor()
         notificationManager = getSystemService()!!
-
         createNotificationChannel()
-
-        val options = EntityExtractorOptions.Builder(EntityExtractorOptions.ENGLISH)
-            .build()
-        eventExtractor = EntityExtraction.getClient(options)
-
-        // Download model if needed
-        val modelDownloadConditions = DownloadConditions.Builder()
-            .build()
-        eventExtractor.downloadModelIfNeeded(modelDownloadConditions)
-            .addOnSuccessListener {
-                Log.i("NotiListenerService", "Model downloaded")
-                modelDownloaded.tryEmit(true)
-            }
-            .addOnFailureListener {
-                throw it
-            }
     }
 
     override fun onListenerDisconnected() {
@@ -73,98 +46,44 @@ class NotiEventExtractorService : NotificationListenerService() {
         if (sbn == null) return
         Log.d("NotiEventExtractorService", "Got notification")
         coroutineScope.launch {
-            Log.d("NotiEventExtractorService", "Checking model is downloaded")
-            modelDownloaded.first { it }
-            Log.d("NotiEventExtractorService", "Model downloaded")
             val messageStyle = NotificationCompat.MessagingStyle
                 .extractMessagingStyleFromNotification(sbn.notification)
             val text = if (messageStyle != null) {
-                Log.i("NotiListenerService", "Got MessageStyle notification")
+                Log.d("NotiEventExtractorService", "Got MessageStyle notification")
                 messageStyle.messages.filter { !it.text.isNullOrBlank() }.joinToString { it.text!! }
             } else {
                 Log.d("NotiEventExtractorService", "Got standard notification")
                 sbn.notification.extras.getString(Notification.EXTRA_TEXT, "")
             }
-            val params = EntityExtractionParams.Builder(text)
-                .setEntityTypesFilter(
-                    setOf(
-                        Entity.TYPE_DATE_TIME,
-                        Entity.TYPE_ADDRESS
-                    )
-                )
-                .build()
-            eventExtractor.annotate(params)
-                .addOnSuccessListener {
-                    parseAnnotations(it)
-                }
-        }
-    }
-
-    private fun parseAnnotations(annotations: List<EntityAnnotation>) {
-        Log.d("NotiEventExtractorService", "Got ${annotations.count()} annotations")
-        annotations.forEach { annotation ->
-            val entities = annotation.entities
-            Log.d("NotiEventExtractorService", "Got ${entities.count()} entities")
-            entities.forEach { entity ->
-                when (entity) {
-                    is DateTimeEntity -> {
-                        Log.d("NotiEventExtractorService", "${entity.dateTimeGranularity}")
-                        Log.d("NotiEventExtractorService", "${entity.timestampMillis}")
-                        val endMillis = estimateEndTimeMillis(
-                            entity.dateTimeGranularity, entity.timestampMillis
-                        )
-                        postEventNotification(
-                            entity.timestampMillis,
-                            endMillis
-                        )
-                    }
-                    else -> {
-                        Log.d("NotiEventExtractorService", "$entity")
-                    }
-                }
+            val eventDetails = eventExtractor.extractEventsFrom(text)
+            Log.d("NotiEventExtractorService", "Got ${eventDetails.count()} event details")
+            eventDetails.forEach {
+                postEventNotification(it)
             }
         }
     }
 
-    private fun estimateEndTimeMillis(granularity: Int, startMillis: Long): Long {
-        val durationMillis = when (granularity) {
-            DateTimeEntity.GRANULARITY_DAY -> TimeUnit.DAYS.toMillis(1)
-            DateTimeEntity.GRANULARITY_MONTH -> TimeUnit.DAYS.toMillis(30)
-            DateTimeEntity.GRANULARITY_WEEK -> TimeUnit.DAYS.toMillis(7)
-            DateTimeEntity.GRANULARITY_YEAR -> TimeUnit.DAYS.toMillis(365)
-            else -> TimeUnit.HOURS.toMillis(1)
-        }
-        return startMillis + durationMillis
-    }
-
-    private fun postEventNotification(
-        startMillis: Long,
-        endMillis: Long,
-        location: String? = null
-    ) {
+    private fun postEventNotification(eventDetails: EventDetails) {
         val notificationId = idCounter.incrementAndGet()
         val createEventIntent = Intent(Intent.ACTION_INSERT)
             .setData(CalendarContract.Events.CONTENT_URI)
-            .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startMillis)
-            .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endMillis)
-            .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+            .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, eventDetails.start.time)
+            .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, eventDetails.end.time)
         val createPendingIntent = PendingIntent.getActivity(
             this, notificationId, createEventIntent, PendingIntent.FLAG_IMMUTABLE
         )
 
         val dateFormatter = DateFormat.getDateTimeInstance()
+        val formattedStart = dateFormatter.format(eventDetails.start)
+        val formattedEnd = dateFormatter.format(eventDetails.end)
 
-        val startDateTime = dateFormatter.format(Date(startMillis))
         val detailString = StringBuilder()
-        detailString.appendLine(getString(R.string.event_from, startDateTime))
-        detailString.appendLine(getString(R.string.event_to, dateFormatter.format(Date(endMillis))))
-        location?.let {
-            detailString.append(getString(R.string.event_at, location))
-        }
+        detailString.appendLine(getString(R.string.event_from, formattedStart))
+        detailString.appendLine(getString(R.string.event_to, formattedEnd))
 
         val notification = Notification.Builder(this, EventDetailsChannel)
             .setContentTitle(getString(R.string.event_noti_title))
-            .setContentText(getString(R.string.event_noti_summary, startDateTime))
+            .setContentText(getString(R.string.event_noti_summary, formattedStart))
             .setSmallIcon(R.drawable.noti_ic_event_found)
             .setStyle(
                 Notification.BigTextStyle().bigText(detailString.toString())
