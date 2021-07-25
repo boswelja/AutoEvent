501f9a1e8fc712fd1801fc10fe97fb1b1f2dc464
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-07-25 17:12:25 +1200

    Expose settings in NotiExtractorSettingsViewModel.kt

--- app/src/main/kotlin/com/boswelja/autoevent/notificationeventextractor/ui/NotiExtractorSettingsViewModel.kt
+++ app/src/main/kotlin/com/boswelja/autoevent/notificationeventextractor/ui/NotiExtractorSettingsViewModel.kt
@@ -3,13 +3,20 @@ package com.boswelja.autoevent.notificationeventextractor.ui
 import android.app.Application
 import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.boswelja.autoevent.notificationeventextractor.notiExtractorSettingsStore
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 class NotiExtractorSettingsViewModel(
     application: Application
 ) : AndroidViewModel(application) {
 
+    private val dataStore = application.notiExtractorSettingsStore
+
     val serviceEnabled = MutableStateFlow(false)
+    val blocklist = dataStore.data.map { it.blocklist }
 
     init {
         updateServiceStatus()
@@ -21,5 +28,11 @@ class NotiExtractorSettingsViewModel(
             .getEnabledListenerPackages(context)
             .contains(context.packageName)
         serviceEnabled.tryEmit(isServiceEnabled)
+    }
+
+    fun updateBlocklist(newList: List<String>) {
+        viewModelScope.launch {
+            dataStore.updateData { it.copy(blocklist = newList) }
+        }
     }
 }
