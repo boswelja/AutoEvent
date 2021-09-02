374b109e0009fb1749a7ec82a65e15d36f39183c
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-09-02 19:41:39 +1200

    Add shortcuts to the app source code and privacy policy

--- app/src/main/kotlin/com/boswelja/autoevent/about/ui/AboutApp.kt
+++ app/src/main/kotlin/com/boswelja/autoevent/about/ui/AboutApp.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Icon
 import androidx.compose.material.ListItem
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PrivacyTip
 import androidx.compose.material.icons.filled.RateReview
+import androidx.compose.material.icons.filled.Source
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -22,6 +24,7 @@ fun AboutApp(
 ) {
     Column(modifier) {
         SupportOptions()
+        InfoOptions()
     }
 }
 
@@ -42,6 +45,38 @@ fun SupportOptions(
             },
             text = { Text(stringResource(R.string.rate_app)) },
             icon = { Icon(Icons.Default.RateReview, null) }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun InfoOptions(
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+    Column(modifier) {
+        ListItem(
+            modifier = Modifier.clickable {
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://github.com/boswelja/AutoEvent")
+                )
+                context.startActivity(intent)
+            },
+            text = { Text(stringResource(R.string.source_code)) },
+            icon = { Icon(Icons.Default.Source, null) }
+        )
+        ListItem(
+            modifier = Modifier.clickable {
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://github.com/boswelja/AutoEvent/blob/main/PRIVACY.md")
+                )
+                context.startActivity(intent)
+            },
+            text = { Text(stringResource(R.string.privacy_policy)) },
+            icon = { Icon(Icons.Default.PrivacyTip, null) }
         )
     }
 }
