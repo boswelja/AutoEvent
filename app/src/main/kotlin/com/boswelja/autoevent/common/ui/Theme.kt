c5ee62c7dcf7c97e0c97f615cfa63bc7db11a5fa
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-03-28 20:45:17 +1300

    Update non-dynamic color palette

--- app/src/main/kotlin/com/boswelja/autoevent/common/ui/Theme.kt
+++ app/src/main/kotlin/com/boswelja/autoevent/common/ui/Theme.kt
@@ -1,6 +1,5 @@
 package com.boswelja.autoevent.common.ui
 
-import android.content.Context
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,8 +15,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-
-const val useMonet = true
 
 val shapes = Shapes(
     small = RoundedCornerShape(50),
@@ -37,59 +34,11 @@ fun getColors(darkTheme: Boolean): ColorScheme {
             }
         } else {
             if (darkTheme) {
-                darkColorScheme()
+                DarkColors
             } else {
-                lightColorScheme()
+                LightColors
             }
         }
-    }
-}
-
-private fun Context.getDarkPrimaryColor(): Color {
-    return if (useMonet && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        Color(getColor(android.R.color.system_accent1_200))
-    } else {
-        Color(0xffffab91)
-    }
-}
-
-private fun Context.getLightPrimaryColor(): Color {
-    return if (useMonet && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        Color(getColor(android.R.color.system_accent1_500))
-    } else {
-        Color(0xffff5722)
-    }
-}
-
-private fun Context.getLightBackgroundColor(): Color {
-    return if (useMonet && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        Color(getColor(android.R.color.system_neutral1_50))
-    } else {
-        Color(0xFFFBE9E7)
-    }
-}
-
-private fun Context.getLightSurfaceColor(): Color {
-    return if (useMonet && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        Color(getColor(android.R.color.system_neutral1_10))
-    } else {
-        Color.White
-    }
-}
-
-private fun Context.getDarkBackgroundColor(): Color {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        Color(getColor(android.R.color.system_neutral1_900))
-    } else {
-        Color(0xFF212121)
-    }
-}
-
-private fun Context.getDarkSurfaceColor(): Color {
-    return if (useMonet && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        Color(getColor(android.R.color.system_neutral1_900))
-    } else {
-        Color(0xFF212121)
     }
 }
 
