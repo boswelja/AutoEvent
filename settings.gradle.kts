3e42c7eb42dc396d20e717fec864e850cd6fccba
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-03-28 15:36:03 +1300

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,9 +5,9 @@ pluginManagement {
         mavenCentral()
     }
     plugins {
-        id("com.android.application") version "7.4.1"
-        id("com.android.library") version "7.4.1"
-        id("org.jetbrains.kotlin.android") version "1.8.10"
+        id("com.android.application") version "8.3.1"
+        id("com.android.library") version "8.3.1"
+        id("org.jetbrains.kotlin.android") version "1.9.23"
         id("com.squareup.wire") version "4.5.1"
     }
 }
@@ -20,5 +20,3 @@ dependencyResolutionManagement {
 }
 rootProject.name = "AutoEvent"
 include(":app")
-
-enableFeaturePreview("VERSION_CATALOGS")
