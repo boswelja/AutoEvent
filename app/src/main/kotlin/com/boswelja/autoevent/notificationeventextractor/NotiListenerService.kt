94d6d9035c11bcbfe87164264cb0ef1d3e08f0be
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-03-28 19:30:39 +1300

    Fix service not starting

--- app/src/main/kotlin/com/boswelja/autoevent/notificationeventextractor/NotiListenerService.kt
+++ app/src/main/kotlin/com/boswelja/autoevent/notificationeventextractor/NotiListenerService.kt
@@ -1,8 +1,5 @@
 package com.boswelja.autoevent.notificationeventextractor
 
-import android.content.Intent
-import android.os.Binder
-import android.os.IBinder
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import kotlinx.coroutines.CoroutineScope
@@ -14,8 +11,6 @@ class NotiListenerService : NotificationListenerService() {
     private val coroutineScope = CoroutineScope(Dispatchers.Default)
 
     private lateinit var notiExtractor: NotiEventExtractor
-
-    override fun onBind(intent: Intent?): IBinder = Binder()
 
     override fun onListenerConnected() {
         notiExtractor = NotiEventExtractor(applicationContext)
